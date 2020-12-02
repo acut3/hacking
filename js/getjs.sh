53c78cc8b53cfacd6649ee32936c5ac1955fe514
Nicolas Christin <nic.christin@gmail.com>
2020-12-03 00:01:45 +0100

    getjs.sh: add support for html files

--- js/getjs.sh
+++ js/getjs.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/bash
 
+# Download files
 wget --no-check-certificate --compression=gzip -xi -
-find . -type f -exec js-beautify -r {} \;
+
+# Beautify them
+find . -type f | while read f
+do
+    ftype='js'
+
+    # Files that don't end with .js and that have '<' as their first non-blank
+    # character are HTML
+    echo "$f" | grep -vqE '\.js(\?)?' &&
+        [[ `sed '/\S/{s/\s*\(.\).*/\1/;q};d' "$f"` = '<' ]] &&
+        ftype='html'
+
+    printf '[%4s] ' "$ftype"
+
+    # Remove any leading space on first line
+    sed -i '1s/^\s*//' "$f"
+    # Beautify file
+    js-beautify --type "$ftype" -r "$f"
+done
